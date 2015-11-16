611841078e62df59791fba3953bdf33fd09f8f76
Maks3w <github.maks3w@virtualplanets.net>
2015-11-16 13:00:27 +0100

    cs

--- test/Generator/ValueGeneratorTest.php
+++ test/Generator/ValueGeneratorTest.php
@@ -36,7 +36,7 @@ class ValueGeneratorTest extends \PHPUnit_Framework_TestCase
     {
         $valueGenerator = new ValueGenerator();
         $valueGenerator->setValue('foo');
-        $this->assertEquals('\'foo\'', $valueGenerator->generate());
+        $this->assertEquals("'foo'", $valueGenerator->generate());
     }
 
     public function testPropertyDefaultValueCanHandleArray()
