996db19dee4c7eb4d9809e2e718b89cd565392ef
Matthew Weier O'Phinney <matthew@zend.com>
2016-06-21 08:58:33 -0500

    First publication

---
+++ js/base.js
@@ -0,0 +1,19 @@
+
+/* Highlight */
+$( document ).ready(function() {
+    $('table').addClass('table table-striped table-hover');
+});
+
+
+$('body').scrollspy({
+    target: '.bs-sidebar',
+});
+
+
+/* Prevent disabled links from causing a page reload */
+$("li.disabled a").click(function() {
+    event.preventDefault();
+});
+
+
+
