7b37b62772dbd791362c29372b3858ea7259b95c
kis龍 <pifeifei1989@qq.com>
2019-12-28 14:52:21 +0800

    function addition parameter and document

--- test/TestAsset/functions.php
+++ test/TestAsset/functions.php
@@ -3,14 +3,25 @@
  * Zend Framework (http://framework.zend.com/)
  *
  * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2005-2019 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
 namespace ZendTest\Code\TestAsset;
 
-function foo_bar()
+/**
+ * fun document
+ *
+ * @param $param
+ * @param bool $param2
+ * @return string
+ */
+function foo_bar($param, $param2=true)
 {
+    if(true){$abc='default+';}
+    $abc.=$param.$param2."abc'\"";
+    $fun=function(){};
+    return $abc;
 }
 
 function bar_foo()
